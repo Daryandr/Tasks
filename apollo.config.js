492f5b6fbfd7a3082123184cbb7f4a84c0b2780a
Dasha <andreeva.d.a9@gmail.com>
2023-03-24 19:39:07 +0300

    change apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
         service: {
             name: 'notes',
             // URL to the GraphQL API
-            url: 'https://rickandmortyapi.com/graphql',
+            url: 'https://localhost:4000/graphql',
         },
         // Files processed by the extension
         includes: [
