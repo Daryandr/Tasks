16972ba5edcb855b8c870ee6d7bd5c7a4fecaa52
Dasha <andreeva.d.a9@gmail.com>
2023-03-25 01:29:27 +0300

    edit server

--- server/mockServer.mjs
+++ server/mockServer.mjs
@@ -2,25 +2,45 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { addMocksToSchema } from '@graphql-tools/mock';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import casual from 'casual';
 
 const typeDefs = `#graphql
-type Query {
-    hello: String
-    resolved: String
+type Query{
+    tasklist:TaskList
+}
+type TaskList{
+    tasks: [Task]
+}
+type Task {
+    title:String
+    checklist:[Check]
+}
+type Check{
+    name:String
+    checked:Boolean
 }
 `;
 
 const resolvers = {
-    Query: {
-        resolved: () => 'Resolved',
-    },
+    Query: {},
+    Check:{},
+    Task:{},
+    TaskList:{}
 };
 
 
 const mocks = {
-    Int: () => 6,
-    Float: () => 22.1,
-    String: () => 'Hello',
+    Check: () => ({
+        name: () => casual.title,
+        checked: () => casual.boolean,
+    }),
+    Task: () => ({
+        title: () => casual.title,
+        checklist: [...new Array(casual.integer(1, 10))]
+    }),
+    TaskList: () => ({
+        tasks: [...new Array(casual.integer(1, 10))]
+    }),
 };
 
 
@@ -28,10 +48,9 @@ const server = new ApolloServer({
     schema: addMocksToSchema({
         schema: makeExecutableSchema({ typeDefs, resolvers }),
         mocks,
-        preserveResolvers: true,
     }),
 });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
-console.log(`🚀 Server listening at: ${url}`);
+console.log(`Server listening at: ${url}`);
