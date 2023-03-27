d4efdf9425ee72a698bdf74d0666e91aabbba279
Dasha <andreeva.d.a9@gmail.com>
2023-03-27 20:10:47 +0300

    move TaskList data to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,72 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    tasks:[
+      {title:"task1",
+        checkList:[{
+          name:"check1",
+          checked:false
+        },
+          {
+            name:"check2",
+            checked:true
+          }]
+      },
+      {title:"task2",
+        checkList:[{
+          name:"check1",
+          checked:false
+        },
+          {
+            name:"check2",
+            checked:true
+          },
+          {
+            name:"check3",
+            checked:true
+          },
+          {
+            name:"check4",
+            checked:false
+          }
+          ,
+          {
+            name:"check5",
+            checked:false
+          }]
+      },
+      {title:"task2",
+        checkList:[{
+          name:"check1",
+          checked:false
+        },
+          {
+            name:"check2",
+            checked:true
+          },
+          {
+            name:"check3",
+            checked:true
+          },
+          {
+            name:"check4",
+            checked:true
+          },
+          {
+            name:"check4",
+            checked:true
+          },
+          {
+            name:"check4",
+            checked:true
+          }]
+      }
+    ]
   },
   getters: {
+    allTasks(state){
+      return state.tasks;
+    }
   },
   mutations: {
   },
