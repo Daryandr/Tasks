cf3b56f4af5105e03178bef05c3dc01d226f5bba
Dasha <andreeva.d.a9@gmail.com>
2023-04-08 23:28:16 +0300

    fixed log data mistake

--- server/mockServer.mjs
+++ server/mockServer.mjs
@@ -36,11 +36,11 @@ type Mutation{
 
 const resolvers = {
     Mutation: {
-        deleteTask: (o) => {
+        deleteTask: (_,o) => {
             console.log("Deleted task with index ",o.taskId)
             return `Task deleted`
         },
-        editTask: (o) => {
+        editTask: (_,o) => {
             console.log("Task with index ",o.taskId," edited to ",o.task)
             return `Task edited`
         },
