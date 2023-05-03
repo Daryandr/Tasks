6cb903c1eb5c0df750c6e69ef1f7de932e87b6f0
Dasha <andreeva.d.a9@gmail.com>
2023-05-04 02:29:35 +0300

    add id field

--- src/graphql/tasks.ts
+++ src/graphql/tasks.ts
@@ -3,8 +3,10 @@ import gql from 'graphql-tag'
 export const TASKS = gql`query Query {
     tasklist {
         tasks {
+            id
             title
             checklist {
+                id
                 checked
                 name
             }
