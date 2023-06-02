8cf15864f6048a0dcd5207a7ed24281ccc849407
Dasha <andreeva.d.a9@gmail.com>
2023-06-02 20:08:01 +0300

    fix formatting

--- server/mockServer.js
+++ server/mockServer.js
@@ -1,7 +1,7 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { addMocksToSchema } from '@graphql-tools/mock';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+import {addMocksToSchema} from '@graphql-tools/mock';
+import {makeExecutableSchema} from '@graphql-tools/schema';
 import {typeDefs} from "./types.js";
 import {resolvers} from "./resolvers.js";
 import {mocks} from "./mocks.js";
@@ -9,12 +9,12 @@ import config from "./config.js"
 
 const server = new ApolloServer({
     schema: addMocksToSchema({
-        schema: makeExecutableSchema({ typeDefs, resolvers }),
+        schema: makeExecutableSchema({typeDefs, resolvers}),
         mocks,
         preserveResolvers: true
     }),
 });
 
-const { url } = await startStandaloneServer(server, { listen: { port: config.port } });
+const {url} = await startStandaloneServer(server, {listen: {port: config.port}});
 
 console.log(`Server listening at: ${url}`);
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,15 +1,15 @@
 export const resolvers = {
     Mutation: {
-        deleteTask: (_,o) => {
-            console.log("Deleted task with index ",o.taskId)
+        deleteTask: (_, o) => {
+            console.log("Deleted task with index ", o.taskId)
             return `Task deleted`
         },
-        editTask: (_,o) => {
-            console.log("Task with index ",o.taskId," edited to ",o.task)
+        editTask: (_, o) => {
+            console.log("Task with index ", o.taskId, " edited to ", o.task)
             return `Task edited`
         },
-        addTask: (_,o) => {
-            console.log("Task added ",o.task)
+        addTask: (_, o) => {
+            console.log("Task added ", o.task)
             return `Task added`
         }
     }
